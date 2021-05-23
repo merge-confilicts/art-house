771a1c0fd057167d780527e1a50371971f0063e2
razan-am <razanalamleh@gmail.com>
2021-05-23 18:13:03 +0300

    adding js to the main page

---
+++ js/app.js
@@ -0,0 +1,39 @@
+'use strict';
+
+let btn = document.getElementById('button');
+let xBtn = document.getElementById('xButton');
+btn.addEventListener('click', tourShow);
+xBtn.addEventListener('click', tourShow);
+
+function tourShow(event) {
+    let slideShow = document.getElementById('tour');
+    let displaySetting = slideShow.style.display;
+
+    if (displaySetting == 'block') {
+        slideShow.style.display = 'none';
+        btn.innerHTML = 'tour';
+    } else if (xBtn.addEventListener) {
+        slideShow.style.display = 'block';
+    }
+}
+
+let index = 1;
+showSlide(index);
+function plusSlides(action) {
+    showSlide(index += action);
+}
+
+function showSlide(n) {
+    let slides = document.getElementsByClassName('mySlide');
+    console.log(slides);
+    if (n > slides.length) { index = 1 }
+    if (n < 1) { index = slides.length }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+
+    slides[index-1].style.display = "block";
+
+}
+
