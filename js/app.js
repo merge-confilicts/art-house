fce71801adc4118ff0a63d917396d0e655fc567b
KZTahat <kztahat96@gmail.com>
2021-05-25 20:41:54 +0300

    compiling pages

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ function tourShow(event) {
     let displaySetting = slideShow.style.display;
     if (displaySetting == 'block') {
         slideShow.style.display = 'none';
-        btn.innerHTML = 'tour';
+        // btn.innerHTML = 'Tour';
     } else if (xBtn.addEventListener) {
         slideShow.style.display = 'block';
     }
