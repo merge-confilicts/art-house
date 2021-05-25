fb4aa7989e4bc3bd6b1b0325d337116ae6945a41
razan-am <razanalamleh@gmail.com>
2021-05-26 00:49:22 +0300

    editing the css

---
+++ .history/js/submit_20210525234636.js
@@ -0,0 +1,36 @@
+
+'use strict'
+
+let form = document.getElementById('joinForm');
+let xBtn1 = document.getElementById('xsubmit');
+let msgDiv = document.getElementById('message');
+xBtn1.style.display = 'none';
+form.addEventListener('submit', handelClicks)
+let btn1 = document.getElementById('submitm');
+btn1.style.display = 'none';
+
+function handelClicks(event) {
+    event.preventDefault();
+    xBtn1.style.display = 'block';
+    if (btn1.style.display = 'none') {
+        btn1.style.display = 'block';
+    }
+};
+xBtn1.addEventListener('click', handelClicks1);
+
+function handelClicks1(event) {
+    event.preventDefault();
+
+    if (btn1.style.display = 'block') {
+        btn1.style.display = 'none';
+    }
+    if (xBtn1.style.display = 'block') {
+        xBtn1.style.display = 'none';
+    }
+    if (msgDiv.style.display = 'block') {
+        msgDiv.style.display = 'none';
+    }
+};
+
+
+
