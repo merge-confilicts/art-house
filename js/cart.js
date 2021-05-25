3d97fa5f62e6b473690a7e2c1918e99f48d5afb4
Momansi96 <mansimohammed8@gmail.com>
2021-05-25 19:05:46 +0300

    adding css to cart

--- js/cart.js
+++ js/cart.js
@@ -30,44 +30,43 @@ function okMessage(event) {
 
 
 
-const table = document.getElementById('cart');
-table.addEventListener('click', removeItemFromCart);
+let table = document.getElementById('cart');
+let tBody= table.getElementsByTagName('tbody')[0];
+let tableRow;
 let cart; 
+let cartProduct;
+let cartPrice; 
+function getLocalStorage(){
+  cartProduct = JSON.parse(localStorage.getItem('product')) || []; 
+  console.log(cartProduct); 
+   cartPrice = JSON.parse(localStorage.getItem('price')) || [];
+  console.log(cartPrice); 
+}
 
-const cartProduct = JSON.parse(localStorage.getItem('product')) || []; 
-console.log(cartProduct); 
-const cartPrice = JSON.parse(localStorage.getItem('price')) || [];
-console.log(cartPrice); 
 function renderCart() {
-	// loadCart();
 	clearCart();
 	showCart();
   }
 
-// function loadCart() {
-//   new cartProduct(paints[i], cartPrice[i]);
-//   }
-// console.log(cartItems);
 function clearCart() {
-let tBody= table.getElementsByTagName('tbody')[0];
+
 
 while (tBody.firstElementChild){
   tBody.removeChild(tBody.firstElementChild);
 }
 }
-
 function showCart() {
 	let tBody = table.getElementsByTagName('tbody')[0];
 	for (let i=0; i<cartProduct.length;i++){
 		
-	let tableRow = document.createElement('tr');
+	tableRow = document.createElement('tr');
     
 	let deletetd= document.createElement('td');
     deletetd.setAttribute('id',cartProduct[i]);
     let button=document.createElement('p');
     deletetd.appendChild(button);
     button.textContent='X';
-    button.setAttribute('id',cartProduct[i]);
+    button.setAttribute('id',i);
     button.addEventListener('click',removeItemFromCart);
   
     let nameProducttd= document.createElement('td');
@@ -85,15 +84,29 @@ function showCart() {
 
 function removeItemFromCart(event) {
 	let removeItem= event.target.id;
-	for (let i=0; i<cartProduct.length;i++){
-	  if (cartProduct[i].product===removeItem){
-      cartProduct.removeItem (cartProduct[i]);
-      cartPrice.removeItem (cartPrice[i]);
-	}}
-
-	// cartProduct.saveToLocalStorage();
-  // price.saveToLocalStorage(); 
-    renderCart();
+  let removeArray = []; 
+	let removeArray2 = []; 
+for (let i=0; i<cartProduct.length; i++){
+if (i !== Number(removeItem)){
+removeArray.push(cartProduct[i]); 
+removeArray2.push(cartPrice[i]); 
 }
-
+}
+cartProduct = removeArray; 
+cartPrice = removeArray2; 
+console.log(cartProduct); 
+console.log(removeArray); 
+tableRow.innerHTML=""; 
+setToLocal();
+getLocalStorage(); 
+renderCart();
+}
+function setToLocal() {
+let item = JSON.stringify(cartProduct); 
+localStorage.setItem('product', item); 
+let item2 = JSON.stringify(cartPrice); 
+localStorage.setItem('price', item2); 
+}
+	
+getLocalStorage(); 
 renderCart();
